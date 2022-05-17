1ff50d6da478633ca20b33da453e357298644770
Fabio Bustos <95886463+fabiobustos-sanjuan@users.noreply.github.com>
2022-05-17 17:31:10 -0300

    agregando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { LogoComponent } from './componentes/logo/logo.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { FormacionComponent } from './componentes/formacion/formacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    LogoComponent,
+    AcercaDeComponent,
+    FormacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
